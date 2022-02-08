329be3b307a2b472594e976b937439cc312488ed
Michael Aharoni <michaelaharoni001@gmail.com>
2022-02-08 17:13:04 +0200

    55555

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import imgBg from '../src/bg.jpg'
 import { AppHeader } from './cmps/AppHeader.jsx';
 import { HomePage } from './pages/HomePage.jsx';
 import { SecondPage } from './pages/SecondPage.jsx';
@@ -13,6 +13,7 @@ export function RootCmp() {
       <BrowserRouter>
         <AppHeader />
         <main className='main-app'>
+          <img className='bg-img' src={imgBg} alt="background image" />
           <Routes>
             <Route path='/userlist' element={<UserList />} />
             <Route path='/secondpage' element={<SecondPage />} />
